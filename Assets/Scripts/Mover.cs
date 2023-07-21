615b85ef7a64062e2393808360ac48dad3f5caf7
Daniel Ting <harmeyforever5435@gmail.com>
2023-07-21 18:23:06 -0400

    Added Raycasting debugging

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,8 +5,14 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private NavMeshAgent agent;
+    private Ray lastRay;
 
     private void Update() {
+        if (Input.GetMouseButton(0)) { 
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        
         agent.destination = target.position;
     }
 }
