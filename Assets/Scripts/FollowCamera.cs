9fe758c392e84241f13fd74d29cc57f629c5a69c
Daniel Ting <harmeyforever5435@gmail.com>
2023-07-22 15:17:47 -0400

    Refined movement

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
-    private void Update() {
+    private void LateUpdate() {
         transform.position = target.position;
     }
 }
