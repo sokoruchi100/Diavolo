f7bd1cabbd066ce30d18cc5f095c0254df8478e4
Daniel Ting <harmeyforever5435@gmail.com>
2023-07-22 22:07:13 -0400

    added basic combat logic

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RPG.Combat {
+    public class Fighter : MonoBehaviour {
+        public void Attack(CombatTarget target) {
+            Debug.Log("EAT THIS DEBUG LINE");
+        }
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,23 +1,45 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control {
     public class PlayerController : MonoBehaviour {
         [SerializeField] private Mover mover;
+        [SerializeField] private Fighter fighter;
 
         private void Update() {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat() {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits) {
+                if (!hit.transform.TryGetComponent(out CombatTarget target)) { continue; }
+
+                if (Input.GetMouseButtonDown(0)) {
+                    fighter.Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement() {
             if (Input.GetMouseButton(0)) {
                 MoveToCursor();
             }
         }
 
         private void MoveToCursor() {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit) {
                 mover.MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay() {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
