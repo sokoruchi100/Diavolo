23385b8dfc7cbc1531368083e9b0beb1e2c4f511
Daniel Ting <harmeyforever5435@gmail.com>
2023-07-24 18:24:10 -0400

    added death anim trigger

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 
 namespace RPG.Combat {
     public class Health : MonoBehaviour {
-        [SerializeField] private float health = 100f;
+        [Header("References")]
+        [SerializeField] private Animator animator;
+
+        [Header("Settings")]
+        [SerializeField] private float healthPoints = 100f;
+        
+        private bool isDead = false;
 
         public void TakeDamage(float damage) {
-            health = Mathf.Max(health - damage, 0);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
 
-            Debug.Log(health);
+            if (healthPoints == 0) {
+                Die();
+            }
+
+            Debug.Log(healthPoints);
+        }
+
+        private void Die() {
+            if (isDead) { return; }
+
+            animator.SetTrigger("die");
+            isDead = true;
         }
     }
 }
