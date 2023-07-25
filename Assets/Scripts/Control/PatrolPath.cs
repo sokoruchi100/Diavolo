f8965c2b2b4231c1594dcd8c55bc6ba0585dd78b
Daniel Ting <harmeyforever5435@gmail.com>
2023-07-24 21:08:56 -0400

    Added waypoint gizmos

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control {
+    public class PatrolPath : MonoBehaviour {
+        private const float waypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos() {
+            for (int i = 0; i < transform.childCount; i++) {
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+            }
+        }
+    }
+}
